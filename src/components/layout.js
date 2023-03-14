1d04bce6a04fe39b88c310315d1ad2c1fb14dcfc
Phil Gebauer <philgebauer@Phils-Laptop.local>
2023-03-14 07:02:30 -0500

    Update Site TItle

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Phil G';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Phil G';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Phil G - Software Engineer"
         />
         <meta
           property="og:image"
