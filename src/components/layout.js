d81f11f074b072002b7c0ddda287503cbf1a5926
Phil Gebauer <philgebauer@Phils-Laptop.local>
2023-03-14 15:02:51 -0500

    Remove border radious

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
             <Image
               priority
               src="/images/profile_white.png"
-              className={utilStyles.borderCircle}
+              // className={utilStyles.borderCircle}
               height={144}
               width={144}
               alt=""
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <Image
                 priority
                 src="/images/profile_white.png"
-                className={utilStyles.borderCircle}
+                // className={utilStyles.borderCircle}
                 height={108}
                 width={108}
                 alt=""
